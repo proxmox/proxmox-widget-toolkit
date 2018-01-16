bf49d2da0fdc9e748364c1ceb7f81ea32e54f950
Thomas Lamprecht <t.lamprecht@proxmox.com>
2018-01-16 14:20:48 +0100

    TimeEdit: use schematic declaration
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- node/TimeEdit.js
+++ node/TimeEdit.js
@@ -2,37 +2,36 @@ Ext.define('Proxmox.node.TimeEdit', {
     extend: 'Proxmox.window.Edit',
     alias: ['widget.proxmoxNodeTimeEdit'],
 
+    subject: gettext('Time zone'),
+
+    width: 400,
+
+    autoLoad: true,
+
+    fieldDefaults: {
+	labelWidth: 70
+    },
+
+    items: {
+	xtype: 'combo',
+	fieldLabel: gettext('Time zone'),
+	name: 'timezone',
+	queryMode: 'local',
+	store: Ext.create('Proxmox.data.TimezoneStore'),
+	displayField: 'zone',
+	forceSelection: true,
+	editable: false,
+	allowBlank: false
+    },
+
     initComponent : function() {
 	var me = this;
 
 	if (!me.nodename) {
 	    throw "no node name specified";
 	}
-
-	Ext.applyIf(me, {
-	    subject: gettext('Time zone'),
-	    url: "/api2/extjs/nodes/" + me.nodename + "/time",
-	    fieldDefaults: {
-		labelWidth: 70
-            },
-	    width: 400,
-	    items: {
-		xtype: 'combo',
-		fieldLabel: gettext('Time zone'),
-		name: 'timezone',
-		queryMode: 'local',
-		store: Ext.create('Proxmox.data.TimezoneStore'),
-		valueField: 'zone',
-		displayField: 'zone',
-		triggerAction: 'all',
-		forceSelection: true,
-		editable: false,
-		allowBlank: false
-	    }
-	});
+	me.url = "/api2/extjs/nodes/" + me.nodename + "/time";
 
 	me.callParent();
-
-	me.load();
     }
 });
