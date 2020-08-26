a718654e761c0f4ecdce7fc846a823ed2504c25a
Thomas Lamprecht <t.lamprecht@proxmox.com>
2020-08-26 20:02:35 +0200

    utils: improve indentation and better check API result
    to avoid dereferencing a null/undefined value.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/Utils.js
+++ src/Utils.js
@@ -449,32 +449,35 @@ utilities: {
     },
 
     checked_command: function(orig_cmd) {
-	Proxmox.Utils.API2Request({
-	    url: '/nodes/localhost/subscription',
-	    method: 'GET',
-	    failure: function(response, opts) {
-		Ext.Msg.alert(gettext('Error'), response.htmlStatus);
+	Proxmox.Utils.API2Request(
+	    {
+		url: '/nodes/localhost/subscription',
+		method: 'GET',
+		failure: function(response, opts) {
+		    Ext.Msg.alert(gettext('Error'), response.htmlStatus);
+		},
+		success: function(response, opts) {
+		    let res = response.result;
+		    if (res === null || res === undefined || !res || res
+			.data.status !== 'Active') {
+			Ext.Msg.show({
+			    title: gettext('No valid subscription'),
+			    icon: Ext.Msg.WARNING,
+			    message: Proxmox.Utils.getNoSubKeyHtml(res.data.url),
+			    buttons: Ext.Msg.OK,
+			    callback: function(btn) {
+				if (btn !== 'ok') {
+				    return;
+				}
+				orig_cmd();
+			    },
+			});
+		    } else {
+			orig_cmd();
+		    }
+		},
 	    },
-	    success: function(response, opts) {
-		let data = response.result.data;
-		if (data.status !== 'Active') {
-		    Ext.Msg.show({
-			title: gettext('No valid subscription'),
-			icon: Ext.Msg.WARNING,
-			message: Proxmox.Utils.getNoSubKeyHtml(data.url),
-			buttons: Ext.Msg.OK,
-			callback: function(btn) {
-			    if (btn !== 'ok') {
-				return;
-			    }
-			    orig_cmd();
-			},
-		    });
-		} else {
-		    orig_cmd();
-		}
-	    },
-	});
+	);
     },
 
     assemble_field_data: function(values, data) {
