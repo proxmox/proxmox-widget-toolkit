10a6a6d5ef42f687742fca97065cf43a0eb1b2b1
Dominik Csapak <d.csapak@proxmox.com>
2021-05-26 10:58:36 +0200

    Toolkit: remove firefox touchscreen override
    seems to be fixed, at least i could not reproduce here.
    If users report this again, we can still revert it if necessary.
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/Toolkit.js
+++ src/Toolkit.js
@@ -184,49 +184,6 @@ Ext.apply(Ext.form.field.VTypes, {
     },
 });
 
-// Firefox 52+ Touchscreen bug
-// see https://www.sencha.com/forum/showthread.php?336762-Examples-don-t-work-in-Firefox-52-touchscreen/page2
-// and https://bugzilla.proxmox.com/show_bug.cgi?id=1223
-Ext.define('EXTJS_23846.Element', {
-    override: 'Ext.dom.Element',
-}, function(Element) {
-    let supports = Ext.supports,
-        proto = Element.prototype,
-        eventMap = proto.eventMap,
-        additiveEvents = proto.additiveEvents;
-
-    if (Ext.os.is.Desktop && supports.TouchEvents && !supports.PointerEvents) {
-        eventMap.touchstart = 'mousedown';
-        eventMap.touchmove = 'mousemove';
-        eventMap.touchend = 'mouseup';
-        eventMap.touchcancel = 'mouseup';
-
-        additiveEvents.mousedown = 'mousedown';
-        additiveEvents.mousemove = 'mousemove';
-        additiveEvents.mouseup = 'mouseup';
-        additiveEvents.touchstart = 'touchstart';
-        additiveEvents.touchmove = 'touchmove';
-        additiveEvents.touchend = 'touchend';
-        additiveEvents.touchcancel = 'touchcancel';
-
-        additiveEvents.pointerdown = 'mousedown';
-        additiveEvents.pointermove = 'mousemove';
-        additiveEvents.pointerup = 'mouseup';
-        additiveEvents.pointercancel = 'mouseup';
-    }
-});
-
-Ext.define('EXTJS_23846.Gesture', {
-    override: 'Ext.event.publisher.Gesture',
-}, function(Gesture) {
-    let gestures = Gesture.instance;
-
-    if (Ext.supports.TouchEvents && !Ext.isWebKit && Ext.os.is.Desktop) {
-        gestures.handledDomEvents.push('mousedown', 'mousemove', 'mouseup');
-        gestures.registerEvents();
-    }
-});
-
 Ext.define('EXTJS_18900.Pie', {
     override: 'Ext.chart.series.Pie',
 
