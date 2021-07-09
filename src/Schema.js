5c085daf0b8b909ffe55d95e240425de97e046ab
Thomas Lamprecht <t.lamprecht@proxmox.com>
2021-07-09 16:11:19 +0200

    add schema and move authDomains there
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ src/Schema.js
@@ -0,0 +1,27 @@
+Ext.define('Proxmox.Schema', { // a singleton
+    singleton: true,
+
+    authDomains: {
+	pam: {
+	    name: 'Linux PAM',
+	    add: false,
+	    edit: false,
+	    pwchange: true,
+	},
+	openid: {
+	    name: gettext('OpenID Connect Server'),
+	    ipanel: 'pmxAuthOpenIDPanel',
+	    add: true,
+	    edit: true,
+	    tfa: false,
+	    pwchange: false,
+	    iconCls: 'pmx-itype-icon-openid-logo',
+	},
+    },
+    // to add or change existing for product specific ones
+    overrideAuthDomains: function(extra) {
+	for (const [key, value] of Object.entries(extra)) {
+	    Proxmox.Utils.authSchema[key] = value;
+	}
+    },
+});
--- src/Utils.js
+++ src/Utils.js
@@ -1151,30 +1151,6 @@ utilities: {
 	return icon;
     },
 
-    authSchema: {
-	pam: {
-	    name: 'Linux PAM',
-	    add: false,
-	    edit: false,
-	    pwchange: true,
-	},
-	openid: {
-	    name: gettext('OpenID Connect Server'),
-	    ipanel: 'pmxAuthOpenIDPanel',
-	    add: true,
-	    edit: true,
-	    tfa: false,
-	    pwchange: false,
-	    iconCls: 'pmx-itype-icon-openid-logo',
-	},
-    },
-
-    // to add or change existing for product specific ones
-    overrideAuthSchema: function(extra) {
-	for (const [key, value] of Object.entries(extra)) {
-	    Proxmox.Utils.authSchema[key] = value;
-	}
-    },
 },
 
     singleton: true,
