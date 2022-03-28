463177281c294ad69f85d00a10450494c2001870
Matthias Heiserer <m.heiserer@proxmox.com>
2022-03-28 15:07:34 +0200

    Buttons: add AltText
    The same code is used once in widget toolkit and twice in PVE already,
    so it makes sense to add it as a separate button.
    
    Signed-off-by: Matthias Heiserer <m.heiserer@proxmox.com>
    Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ src/button/AltText.js
@@ -0,0 +1,22 @@
+Ext.define('Proxmox.button.AltText', {
+    extend: 'Proxmox.button.Button',
+    xtype: 'proxmoxAltTextButton',
+
+    defaultText: "",
+    altText: "",
+
+    listeners: {
+	// HACK: calculate the max button width on first render to avoid toolbar glitches
+	render: function(button) {
+	    let me = this;
+
+	    button.setText(me.altText);
+	    let altWidth = button.getSize().width;
+
+	    button.setText(me.defaultText);
+	    let defaultWidth = button.getSize().width;
+
+	    button.setWidth(defaultWidth > altWidth ? defaultWidth : altWidth);
+	},
+    },
+});
