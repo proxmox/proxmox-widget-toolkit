proxmox/proxmox-widget-toolkit

commit 7e47328692987166f734a0a52466dc4a6046e0a6

Combogrid: clear filter on blur
Previously, deselecting and reselecting the input field
led to the previous filter still being applied, although the
input field was cleared.

Signed-off-by: Matthias Heiserer <m.heiserer@proxmox.com>

src/form/ComboGrid.js
@@ -325,6 +325,7 @@ Ext.define('Proxmox.form.ComboGrid', {
             me.changingFilters = true;
             me.store.removeFilter(filter, true);
             me.changingFilters = false;
+	    me.store.fireEvent('refresh');
         }
     },
 

commit 01a79a97814756d4d7c57efc1f88f1bf3eb8bf9d

form: combo grid: fix selection after filtering
firing 'refresh' in 'clearLocalFilter' was wrong, since that triggers
too often, for example when selecting an entry (since the field lost
focus). This lead to the picker refreshing and not registering the
click and thus not selecting the desired entry.

Instead refresh the view when we really need it: when the picker is shown.
The filter is already gone, but the picker grid does not know this yet,
so we fire the event then.

Fixes: 7e47328 ("Combogrid: clear filter on blur")

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/form/ComboGrid.js
@@ -300,6 +300,7 @@ Ext.define('Proxmox.form.ComboGrid', {
 	//
 	// we save the minheight to reset it after the load
 	picker.on('show', function() {
+	    me.store.fireEvent('refresh');
 	    if (me.enableLoadMask) {
 		me.savedMinHeight = picker.getMinHeight();
 		picker.setMinHeight(100);
@@ -324,7 +325,6 @@ Ext.define('Proxmox.form.ComboGrid', {
 	    me.store.removeFilter(me.queryFilter, true);
 	    me.queryFilter = null;
 	    me.changingFilters = false;
-	    me.store.fireEvent('refresh');
 	}
     },
 