proxmox/proxmox-widget-toolkit

commit 454ff6ea91dd60b9d93a01270dcd34bc54ea4ea5

language selector: increase field width
With the new translations and layout, the width of the dropdown menu
needs to be increased so that the languages are all on the same line.
This width might need to increased in the future if translations were
to create a line break.

Signed-off-by: Noel Ullreich <n.ullreich@proxmox.com>
 [ T: reflow message and reword subject a bit ]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/window/LanguageEdit.js
@@ -38,6 +38,7 @@ Ext.define('Proxmox.window.LanguageEditWindow', {
 	    bind: {
 		value: '{language}',
 	    },
+	    width: 400,
 	},
     ],
     buttons: [

commit 311fb72a272a2c66e611d08e1950cb756f245838

language selector: increase only picker list view
As that's what matters for the selection, having parts of the already
selected language cut off is not really a big deal as either one
knows the language and the initial native part is enough or the
picker needs to be opened for changing selection anyway.

But, we can give the field itself a bit more space by reducing the
label width to 75, which is enough for all languages as grepping our
PO files for "Language" and their translation, it seems that the
widest one comes from Euskara (Basque) with its "Hizkuntza" having 9
characters.

This way we avoid having to many different

Fixes: 454ff6e ("language selector: increase field width")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/form/LanguageSelector.js
@@ -3,4 +3,9 @@ Ext.define('Proxmox.form.LanguageSelector', {
     xtype: 'proxmoxLanguageSelector',
 
     comboItems: Proxmox.Utils.language_array(),
+
+    matchFieldWidth: false,
+    listConfig: {
+	width: 300,
+    },
 });

src/window/LanguageEdit.js
@@ -35,10 +35,10 @@ Ext.define('Proxmox.window.LanguageEditWindow', {
 	{
 	    xtype: 'proxmoxLanguageSelector',
 	    fieldLabel: gettext('Language'),
+	    labelWidth: 75,
 	    bind: {
 		value: '{language}',
 	    },
-	    width: 400,
 	},
     ],
     buttons: [