proxmox/proxmox-widget-toolkit

commit 5cbbb9c44a76cb0c1067df9692290e74c4ee921f

fix #4756: markdown notes: allow any valid URL for a tags
As anchor elements cannot load things into the current browsing
context and are not necessarily more dangerous to users compared to
HTTP(S) links, which we allowed since adding markdown rendering in
the first place.

Allows adding short-cuts for virtual guest resources, like RDP or SSH
links.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/Parser.js
@@ -34,6 +34,7 @@ Ext.define('Proxmox.Markdown', {
 			let url = new URL(value, window.location.origin);
 			if (
 			    _isHTTPLike(url.protocol) ||
+			    node.tagName.toLowerCase() === 'a' ||
 			    (node.tagName.toLowerCase() === 'img' && url.protocol.toLowerCase() === 'data:')
 			) {
 			    node.attributes[i].value = url.href;

commit 89699c6466cfd9cc3a81fbc926b62f122c33c23c

parser: split checking IMG and A tags, make the latter more strict
Split the logic so that each tag is handled explicitly on it's own
if-else branch, which is now safer to do as we default to
allow-only-http-like.

Also address a recently introduced regression from the implementation
of the #4756 where any user that could edit notes could use
javascript: script-urls for XSS purpose to prepare a link that could
leak private user information when another user clicked on it, at
least if they omitted basic sanity checks by looking at the URL
displayed by the browser before.

We have to override a false-positive triggered by a eslint heuristic,
a simple string compression should be always safe.

Fixes: 5cbbb9c ("fix #4756: markdown notes: allow any valid URL for a tags")
Reported-by: Hieu Dang Cong <HieuDC5@fpt.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/Parser.js
@@ -35,11 +35,11 @@ Ext.define('Proxmox.Markdown', {
 		    try {
 			let url = new URL(value, window.location.origin);
 			safeURL = _isHTTPLike(url.protocol);
-			if (
-			    canonicalTagName === 'a' ||
-			    (canonicalTagName === 'img' && url.protocol.toLowerCase() === 'data:')
-			) {
+			if (canonicalTagName === 'img' && url.protocol.toLowerCase() === 'data:') {
 			    safeURL = true;
+			} else if (canonicalTagName === 'a') {
+			    // allow most link protocols so admins can use short-cuts to, e.g., RDP
+			    safeURL = url.protocol.toLowerCase() !== 'javascript:'; // eslint-disable-line no-script-url
 			}
 			if (safeURL) {
 			    node.attributes[i].value = url.href;