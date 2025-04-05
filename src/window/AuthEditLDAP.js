proxmox/proxmox-widget-toolkit

commit 0f8d38b5de166fe59acbb4e6ca24e0ff82247d0c

fix #5379: window: AuthEdit{LDAP, OpenId}: add 'Default realm' checkbox
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

src/window/AuthEditLDAP.js
@@ -83,6 +83,19 @@ Ext.define('Proxmox.panel.LDAPInputPanel', {
 	    allowBlank: false,
 	},
 	{
+	    xtype: 'proxmoxcheckbox',
+	    fieldLabel: gettext('Default realm'),
+	    name: 'default',
+	    value: 0,
+	    cbind: {
+		deleteEmpty: '{!isCreate}',
+	    },
+	    autoEl: {
+		tag: 'div',
+		'data-qtip': gettext('Set realm as default for login'),
+	    },
+	},
+	{
 	    xtype: 'proxmoxtextfield',
 	    fieldLabel: gettext('Base Domain Name'),
 	    name: 'base-dn',
@@ -216,7 +229,6 @@ Ext.define('Proxmox.panel.LDAPInputPanel', {
 	    },
 	},
     ],
-
 });
 
 

src/window/AuthEditOpenId.js
@@ -36,6 +36,19 @@ Ext.define('Proxmox.panel.OpenIDInputPanel', {
 	    allowBlank: false,
 	},
 	{
+	    xtype: 'proxmoxcheckbox',
+	    fieldLabel: gettext('Default realm'),
+	    name: 'default',
+	    value: 0,
+	    cbind: {
+		deleteEmpty: '{!isCreate}',
+	    },
+	    autoEl: {
+		tag: 'div',
+		'data-qtip': gettext('Set realm as default for login'),
+	    },
+	},
+	{
 	    xtype: 'proxmoxtextfield',
 	    fieldLabel: gettext('Client ID'),
 	    name: 'client-id',

commit 395e296ff8585ea5590af9d3a49a42336ad02d08

authentication realms: consistently use title case
Use title-case like _every_ other property of those edit windows
already does. This was visually quite noticeable for the more complex
ones (OIDC or LDAP), which have a few labels with a spaces.

Fixes: 0f8d38b ("fix #5379: window: AuthEdit{LDAP, OpenId}: add 'Default realm' checkbox")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/window/AuthEditLDAP.js
@@ -84,7 +84,7 @@ Ext.define('Proxmox.panel.LDAPInputPanel', {
 	},
 	{
 	    xtype: 'proxmoxcheckbox',
-	    fieldLabel: gettext('Default realm'),
+	    fieldLabel: gettext('Default Realm'),
 	    name: 'default',
 	    value: 0,
 	    cbind: {

src/window/AuthEditOpenId.js
@@ -39,7 +39,7 @@ Ext.define('Proxmox.panel.OpenIDInputPanel', {
 	},
 	{
 	    xtype: 'proxmoxcheckbox',
-	    fieldLabel: gettext('Default realm'),
+	    fieldLabel: gettext('Default Realm'),
 	    name: 'default',
 	    value: 0,
 	    cbind: {

src/window/AuthEditSimple.js
@@ -16,7 +16,7 @@ Ext.define('Proxmox.panel.SimpleRealmInputPanel', {
 	},
 	{
 	    xtype: 'proxmoxcheckbox',
-	    fieldLabel: gettext('Default realm'),
+	    fieldLabel: gettext('Default Realm'),
 	    name: 'default',
 	    value: 0,
 	    deleteEmpty: true,