proxmox/proxmox-widget-toolkit

commit 691f530d81e3c8a3642ea08d2019cce8c83f7ae4

network: optionally show alternative interface names
add a new (hidden by default) column for the interface names, and show
them when editing an existing interface with such alternative names.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20250715090749.1608768-2-d.csapak@proxmox.com

src/node/NetworkEdit.js
@@ -5,6 +5,9 @@ Ext.define('Proxmox.node.NetworkEdit', {
     // Enable to show the VLAN ID field
     enableBridgeVlanIds: false,
 
+    // Show alternative names below normal name
+    showAltNames: false,
+
     initComponent: function () {
         let me = this;
 
@@ -363,6 +366,14 @@ Ext.define('Proxmox.node.NetworkEdit', {
             },
         );
 
+        if (me.showAltNames && !me.isCreate) {
+            column1.push({
+                xtype: 'displayfield',
+                name: 'altnames',
+                fieldLabel: gettext("Alternative Names"),
+            });
+        }
+
         if (me.iftype === 'OVSBond') {
             column1.push(
                 {
@@ -448,6 +459,14 @@ Ext.define('Proxmox.node.NetworkEdit', {
                             me.close();
                         });
                         return;
+                    }
+
+                    if (data.altnames) {
+                        if (Ext.isArray(data.altnames)) {
+                            data.altnames = data.altnames.join('<br>');
+                        }
+                    } else {
+                        me.down('field[name=altnames]').setVisible(false);
                     }
                     me.setValues(data);
                     me.isValid(); // trigger validation

src/node/NetworkView.js
@@ -4,6 +4,7 @@ Ext.define('proxmox-networks', {
         'active',
         'address',
         'address6',
+        'altnames',
         'autostart',
         'bridge_ports',
         'cidr',
@@ -32,6 +33,9 @@ Ext.define('Proxmox.node.NetworkView', {
     types: ['bridge', 'bond', 'vlan', 'ovs'],
 
     showApplyBtn: false,
+
+    // if true, the altnames column will be shown by default
+    showAltNames: false,
 
     // for options passed down to the network edit window
     editOptions: {},
@@ -103,6 +107,7 @@ Ext.define('Proxmox.node.NetworkView', {
                 nodename: me.nodename,
                 iface: rec.data.iface,
                 iftype: rec.data.type,
+                showAltNames: me.showAltNames,
                 ...me.editOptions,
                 listeners: {
                     destroy: () => reload(),
@@ -277,6 +282,21 @@ Ext.define('Proxmox.node.NetworkView', {
                             header: gettext('Name'),
                             sortable: true,
                             dataIndex: 'iface',
+                        },
+                        {
+                            header: gettext("Alternative Names"),
+                            dataIndex: 'altnames',
+                            hidden: !me.showAltNames,
+                            width: 140, // enough space for 'enx<MAC>'
+                            renderer: (value) => {
+                                if (!value) {
+                                    return '';
+                                }
+                                if (Ext.isArray(value)) {
+                                    return value.map(Ext.htmlEncode).join('<br>');
+                                }
+                                return Ext.htmlEncode(value);
+                            },
                         },
                         {
                             header: gettext('Type'),

commit c3bb1497d7f1605a2fe04e89a42a38c0292d8741

network edit: fix hiding of altnames field if disabled by config
Commit 691f530d ("network: optionally show alternative interface
names") introduced the optional altnames field in the network edit
grid. The chosen default configuration however does not even add the
field, therefore setting it to hidden if no altnames data is present
cannot work.

Fix this by only hiding the field if it was actually configured to be
shown in the first place.

Fixes: 691f530d ("network: optionally show alternative interface names")
Signed-off-by: Christian Ebner <c.ebner@proxmox.com>
Link: https://lore.proxmox.com/20250731113015.148881-1-c.ebner@proxmox.com

src/node/NetworkEdit.js
@@ -465,7 +465,7 @@ Ext.define('Proxmox.node.NetworkEdit', {
                         if (Ext.isArray(data.altnames)) {
                             data.altnames = data.altnames.join('<br>');
                         }
-                    } else {
+                    } else if (me.showAltNames && !me.isCreate) {
                         me.down('field[name=altnames]').setVisible(false);
                     }
                     me.setValues(data);