b454ec4d1b575e91851d56a7c31d6d3a7e9fe869
Dominik Csapak <d.csapak@proxmox.com>
2025-11-07 15:03:55 +0100

    rrd metrics: split time-frame and aggregation-type into separate selectors
    Instead of duplicating each time-frame for both available aggregation
    types, split the existing combobox into one that just provides the
    'timeframe' selector and a segmented button with the aggregation types
    (average and maximum), i.e. what RRD calls the consolidation function.
    
    This removes some clutter in the dropdown and allows for a slightly
    more convenient and intuitive UX, as e.g. one now can quickly switch
    between average and maximum data points for the same time period.
    
    This approach was originally developed for PDM in its Yew (rust) based
    UI, but it's easy enough to "port" that change over to the ExtJS based
    UIs.
    
    Suggested-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Link: https://lore.proxmox.com/20251107140400.4016493-1-d.csapak@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/form/RRDTypeSelector.js
+++ src/form/RRDTypeSelector.js
@@ -1,42 +1,27 @@
 Ext.define('Proxmox.form.RRDTypeSelector', {
-    extend: 'Ext.form.field.ComboBox',
+    extend: 'Ext.form.FieldContainer',
     alias: ['widget.proxmoxRRDTypeSelector'],
 
-    displayField: 'text',
-    valueField: 'id',
-    editable: false,
-    queryMode: 'local',
-    value: 'hour',
-    stateEvents: ['select'],
     stateful: true,
     stateId: 'proxmoxRRDTypeSelection',
-    store: {
-        type: 'array',
-        fields: ['id', 'timeframe', 'cf', 'text'],
-        data: [
-            ['hour', 'hour', 'AVERAGE', gettext('Hour') + ' (' + gettext('average') + ')'],
-            ['hourmax', 'hour', 'MAX', gettext('Hour') + ' (' + gettext('maximum') + ')'],
-            ['day', 'day', 'AVERAGE', gettext('Day') + ' (' + gettext('average') + ')'],
-            ['daymax', 'day', 'MAX', gettext('Day') + ' (' + gettext('maximum') + ')'],
-            ['week', 'week', 'AVERAGE', gettext('Week') + ' (' + gettext('average') + ')'],
-            ['weekmax', 'week', 'MAX', gettext('Week') + ' (' + gettext('maximum') + ')'],
-            ['month', 'month', 'AVERAGE', gettext('Month') + ' (' + gettext('average') + ')'],
-            ['monthmax', 'month', 'MAX', gettext('Month') + ' (' + gettext('maximum') + ')'],
-            ['year', 'year', 'AVERAGE', gettext('Year') + ' (' + gettext('average') + ')'],
-            ['yearmax', 'year', 'MAX', gettext('Year') + ' (' + gettext('maximum') + ')'],
-        ],
-    },
+    layout: 'hbox',
+
+    referenceHolder: true,
+
     // save current selection in the state Provider so RRDView can read it
     getState: function () {
-        let ind = this.getStore().findExact('id', this.getValue());
-        let rec = this.getStore().getAt(ind);
-        if (!rec) {
-            return undefined;
+        let me = this;
+        let timeframe = me.lookup('timeframe').getValue();
+        let max = me.lookup('maximum').pressed;
+        let id = timeframe;
+        if (max) {
+            id += 'max';
         }
+
         return {
-            id: rec.data.id,
-            timeframe: rec.data.timeframe,
-            cf: rec.data.cf,
+            id,
+            timeframe,
+            cf: max ? 'MAX' : 'AVERAGE',
         };
     },
     // set selection based on last saved state
@@ -45,4 +30,68 @@ Ext.define('Proxmox.form.RRDTypeSelector', {
             this.setValue(state.id);
         }
     },
+
+    setValue: function (value) {
+        let me = this;
+        let timeframe = value;
+        let max = value.endsWith('max');
+        if (max) {
+            timeframe = value.substring(0, value.length - 3);
+        }
+
+        me.lookup('timeframe').setValue(timeframe);
+        me.lookup(max ? 'maximum' : 'average').setPressed(true);
+        me.lookup(!max ? 'maximum' : 'average').setPressed(false);
+    },
+
+    items: [
+        {
+            xtype: 'combobox',
+            reference: 'timeframe',
+            padding: '0 5 0 0',
+            displayField: 'text',
+            valueField: 'id',
+            editable: false,
+            queryMode: 'local',
+            value: 'hour',
+            store: {
+                type: 'array',
+                fields: ['id', 'text'],
+                data: [
+                    ['hour', gettext('Hour')],
+                    ['day', gettext('Day')],
+                    ['week', gettext('Week')],
+                    ['month', gettext('Month')],
+                    ['year', gettext('Year')],
+                ],
+            },
+            listeners: {
+                change: function () {
+                    this.up('proxmoxRRDTypeSelector').saveState();
+                },
+            },
+        },
+        {
+            xtype: 'segmentedbutton',
+            allowMultiple: false,
+            allowToggle: true,
+            items: [
+                {
+                    text: gettext('Maximum'),
+                    reference: 'maximum',
+                    handler: function () {
+                        this.up('proxmoxRRDTypeSelector').saveState();
+                    },
+                },
+                {
+                    text: gettext('Average'),
+                    reference: 'average',
+                    pressed: true,
+                    handler: function () {
+                        this.up('proxmoxRRDTypeSelector').saveState();
+                    },
+                },
+            ],
+        },
+    ],
 });
