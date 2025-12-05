proxmox/proxmox-widget-toolkit

commit 54a2e4173c47e7891af9cbbbdbf4f976a924fc7c

RRD metric type selector: adapt applying CSS class to fix ExtJS debug mode
For some reason, using a space separated list of classes in `itemCls`
breaks the extjs debug mode. This is seemingly allowed everywhere
else, so to fix it, simply apply the classes directly to the children
with 'default'.

An alternative would be to switch itemCls over to an array, but using
a default here is actually a bit more telling and more importantly,
avoids accessing some internal property that isn't really documented.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20251205143450.2953331-1-d.csapak@proxmox.com
 [TL: add some more context from my time running into this]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/form/RRDTypeSelector.js
@@ -75,7 +75,9 @@ Ext.define('Proxmox.form.RRDTypeSelector', {
             xtype: 'segmentedbutton',
             allowMultiple: false,
             allowToggle: true,
-            itemCls: 'x-btn-default-toolbar-small proxmox-inline-button',
+            defautl: {
+                cls: 'x-btn-default-toolbar-small proxmox-inline-button',
+            },
             items: [
                 {
                     text: gettext('Maximum'),

commit f7e555bd05eb41396f0d0bf144a384fa9b3acadf

RRD metric type selector: fix typo
Reported-by: Max Carrara <m.carrara@proxmox.com>
Fixes: 54a2e41 ("RRD metric type selector: adapt applying CSS class to fix ExtJS debug mode")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/form/RRDTypeSelector.js
@@ -75,7 +75,7 @@ Ext.define('Proxmox.form.RRDTypeSelector', {
             xtype: 'segmentedbutton',
             allowMultiple: false,
             allowToggle: true,
-            defautl: {
+            default: {
                 cls: 'x-btn-default-toolbar-small proxmox-inline-button',
             },
             items: [