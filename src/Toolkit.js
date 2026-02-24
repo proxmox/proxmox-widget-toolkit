proxmox/proxmox-widget-toolkit

commit 57cd471f79ecd6b98c9cadb600ba0255360980fe

prevent interpreting of HTML in column grids by default
instead of having to write this for each column individually, just set
it as default for grid column renderers.

Testing here showed that it does not incur a major
performance penalty. During rendering of a grid with ~70 rows and 30
columns (so ~2100 cells), the spent time in htmlEncode was ~0.3ms or
less than 1% of the total time used to render the grid.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/Toolkit.js
@@ -767,6 +767,14 @@ Ext.define('Proxmox.Cookies', {
     },
 });
 
+// Prevents html entities to be interpreted by the browser in grid columns by
+// default. Custom renderer have to take care of this by themselves.
+Ext.define('Proxmox.grid.column.Column', {
+    override: 'Ext.grid.column.Column',
+
+    renderer: Ext.htmlEncode,
+});
+
 // force alert boxes to be rendered with an Error Icon
 // since Ext.Msg is an object and not a prototype, we need to override it
 // after the framework has been initiated

commit fb8da23d5955780d0018d69305c87deb3ab3a596

fix regression in checkcolumn rendering
in commit 57cd471f79ecd6b98c9cadb600ba0255360980fe the default column
renderer was changed to Ext.htmlEncode, so that by default, we don't
return raw HTML there anymore.

The 'checkcolumn' relies on that behavior though, so we have to reset
the renderer there to 'undefined' for it to work properly.

This fixes all uses of 'checkcolumn', for example the enabled column in
the boot order edit window for virtual machines.

Fixes: 57cd471 (prevent interpreting of HTML in column grids by default)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Michael Köppl <m.koeppl@proxmox.com>
Link: https://lore.proxmox.com/all/20260224085733.594116-1-d.csapak@proxmox.com

src/Toolkit.js
@@ -769,10 +769,19 @@ Ext.define('Proxmox.Cookies', {
 
 // Prevents html entities to be interpreted by the browser in grid columns by
 // default. Custom renderer have to take care of this by themselves.
+//
+// Reset the renderer for the 'checkcolumn' back to undefined, since it relies
+// on the renderer returning raw HTML.
 Ext.define('Proxmox.grid.column.Column', {
     override: 'Ext.grid.column.Column',
 
     renderer: Ext.htmlEncode,
+});
+
+Ext.define('Proxmox.grid.column.Check', {
+    override: 'Ext.grid.column.Column',
+
+    renderer: undefined,
 });
 
 // force alert boxes to be rendered with an Error Icon