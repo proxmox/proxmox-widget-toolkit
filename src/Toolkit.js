proxmox/proxmox-widget-toolkit

commit fb8da23d5955780d0018d69305c87deb3ab3a596

fix regression in checkcolumn rendering
in commit 57cd471f79ecd6b98c9cadb600ba0255360980fe the default column
renderer was changed to Ext.htmlEncode, so that by default, we don't
return raw HTML there anymore.

The 'checkcolumn' relies on that behavior though, so we have to reset
the renderer there to 'undefined' for it to work properly.

This fixes all uses of 'checkcolumn', for example the enabled column in
the boot order edit window for virtual machines.

Fixes: 57cd471 (prevent interpreting of HTML in column grids by default)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Michael Köppl <m.koeppl@proxmox.com>
Link: https://lore.proxmox.com/all/20260224085733.594116-1-d.csapak@proxmox.com

src/Toolkit.js
@@ -769,10 +769,19 @@ Ext.define('Proxmox.Cookies', {
 
 // Prevents html entities to be interpreted by the browser in grid columns by
 // default. Custom renderer have to take care of this by themselves.
+//
+// Reset the renderer for the 'checkcolumn' back to undefined, since it relies
+// on the renderer returning raw HTML.
 Ext.define('Proxmox.grid.column.Column', {
     override: 'Ext.grid.column.Column',
 
     renderer: Ext.htmlEncode,
+});
+
+Ext.define('Proxmox.grid.column.Check', {
+    override: 'Ext.grid.column.Column',
+
+    renderer: undefined,
 });
 
 // force alert boxes to be rendered with an Error Icon

commit 3daa89ac9eae58e2623e3726f99d6ebdf13a83b1

fix regression in column renderer (again)
The regression fix for the check column had an error so that the
original change was reverted instead of only modifying the affected
column. Fixing this mistake revealed that we also have to revert the
renderer for other typed columns of ExtJs.

Fixes: fb8da23 (fix regression in checkcolumn rendering)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/Toolkit.js
@@ -770,8 +770,9 @@ Ext.define('Proxmox.Cookies', {
 // Prevents html entities to be interpreted by the browser in grid columns by
 // default. Custom renderer have to take care of this by themselves.
 //
-// Reset the renderer for the 'checkcolumn' back to undefined, since it relies
-// on the renderer returning raw HTML.
+// Reset the renderer for the 'check', 'boolean', 'number' and 'date' columns
+// back to undefined, since they rely on the renderer being undefined or
+// returning raw HTML.
 Ext.define('Proxmox.grid.column.Column', {
     override: 'Ext.grid.column.Column',
 
@@ -779,7 +780,25 @@ Ext.define('Proxmox.grid.column.Column', {
 });
 
 Ext.define('Proxmox.grid.column.Check', {
-    override: 'Ext.grid.column.Column',
+    override: 'Ext.grid.column.Check',
+
+    renderer: undefined,
+});
+
+Ext.define('Proxmox.grid.column.Boolean', {
+    override: 'Ext.grid.column.Boolean',
+
+    renderer: undefined,
+});
+
+Ext.define('Proxmox.grid.column.Number', {
+    override: 'Ext.grid.column.Number',
+
+    renderer: undefined,
+});
+
+Ext.define('Proxmox.grid.column.Date', {
+    override: 'Ext.grid.column.Date',
 
     renderer: undefined,
 });